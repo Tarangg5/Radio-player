82ac8046907efda55e78753f277e85bfdcdd9468
Tarangg5 <sonuujamuicamp@gmail.com>
2026-03-13 00:14:33 +0530

    Create metadata.js

---
+++ radio/api/metadata.js
@@ -0,0 +1,11 @@
+// api/metadata.js
+export default async function handler(req, res) {
+    const { id } = req.query;
+    try {
+        const response = await fetch(`https://api.zeno.fm/mounts/metadata/subscribe/${id}`);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch' });
+    }
+}
